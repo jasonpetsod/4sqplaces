88bec3f9204ec4c498690c1a64e824f18c29b686
Jason Petsod <jason@petsod.org>
2014-08-19 22:49:22 -0400

    Implement list pagination

--- src/static/js/places.js
+++ src/static/js/places.js
@@ -1,6 +1,7 @@
 var places = {}
 
 places.FOURSQUARE_API_VERSION = "20131201";
+places.LIST_PAGINATION_LIMIT = 100;
 
 var places_app = angular.module(
         'places_app',
@@ -39,35 +40,39 @@ places_app.controller('PlacesController', function ($scope, $http) {
     }
 
     $scope.updateMap = function () {
-        // TODO: Implement pagination.
-        $http({
-            method: 'GET',
-            url: 'https://api.foursquare.com/v2/lists/' + $scope.active_list.id,
-            params: {
-                oauth_token: places.OAUTH_TOKEN,
-                v: places.FOURSQUARE_API_VERSION,
-                limit: 100,
-                sort: 'nearby'
-                // TODO: Add bounds.
-            }
-        }).
-        success(function (data) {
-            $scope.venues = [];
-            angular.forEach(data.response.list.listItems.items, function (v, k) {
-                var venue = v.venue;
-                angular.forEach(venue.categories, function (c, _) {
-                    if (c.primary === true) {
-                        venue._places_primary_category = c;
-                    }
+        $scope.venues = [];
+
+        for (var offset = 0;
+             offset < $scope.active_list.listItems.count;
+             offset += places.LIST_PAGINATION_LIMIT) {
+            console.log('offset', offset);
+            $http({
+                method: 'GET',
+                url: 'https://api.foursquare.com/v2/lists/' + $scope.active_list.id,
+                params: {
+                    oauth_token: places.OAUTH_TOKEN,
+                    v: places.FOURSQUARE_API_VERSION,
+                    limit: places.LIST_PAGINATION_LIMIT,
+                    offset: offset,
+                    sort: 'nearby'
+                }
+            }).
+            success(function (data) {
+                angular.forEach(data.response.list.listItems.items, function (v, k) {
+                    var venue = v.venue;
+                    angular.forEach(venue.categories, function (c, _) {
+                        if (c.primary === true) {
+                            venue._places_primary_category = c;
+                        }
+                    });
+                    venue.location.latitude = venue.location.lat;
+                    venue.location.longitude = venue.location.lng;
+                    $scope.venues.push(venue);
                 });
-                venue.location.latitude = venue.location.lat;
-                venue.location.longitude = venue.location.lng;
-                $scope.venues.push(venue);
+            }).
+            error(function (data) {
+                console.log('error', data);
             });
-            console.log($scope.venues);
-        }).
-        error(function (data) {
-            console.log('error', error);
-        });
+        }
     };
 });
