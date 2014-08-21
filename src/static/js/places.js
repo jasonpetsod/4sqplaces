e986bbaf98cd902f96d4e1e917df15f925fa58d1
Jason Petsod <jason@petsod.org>
2014-08-20 22:23:36 -0400

    Move venues query to a service

--- src/static/js/places.js
+++ src/static/js/places.js
@@ -3,6 +3,7 @@ var places = {}
 places.FOURSQUARE_API_VERSION = "20131201";
 places.LIST_PAGINATION_LIMIT = 100;
 places.LISTS_API_URI = 'https://api.foursquare.com/v2/users/self/lists';
+places.VENUES_API_URI = 'https://api.foursquare.com/v2/lists/';
 
 var places_app = angular.module(
     'places_app',
@@ -37,7 +38,47 @@ places_app.factory('listsService', function ($http) {
     };
 });
 
-places_app.controller('PlacesController', function ($scope, $http, $location, $anchorScroll, listsService) {
+places_app.factory('venuesService', function ($http) {
+    return {
+        getVenues: function (list_id, offset) {
+            var success = function (data) {
+                var venues = [];
+                var items = data.data.response.list.listItems.items;
+                angular.forEach(items, function (v, k) {
+                    var venue = v.venue;
+                    // TODO: Make a Venue class.
+                    angular.forEach(venue.categories, function (c, _) {
+                        if (c.primary === true) {
+                            venue._places_primary_category = c;
+                        }
+                    });
+                    venue.location.latitude = venue.location.lat;
+                    venue.location.longitude = venue.location.lng;
+                    venue.location.latlng = new google.maps.LatLng(
+                        venue.location.lat, venue.location.lng);
+                    venues.push(venue);
+                });
+                return venues;
+            };
+            var error = function (data) {
+                return data;
+            };
+
+            var config = {
+                params: {
+                    oauth_token: places.OAUTH_TOKEN,
+                    v: places.FOURSQUARE_API_VERSION,
+                    limit: places.LIST_PAGINATION_LIMIT,
+                    offset: offset,
+                }
+            };
+            var url = places.VENUES_API_URI + list_id;
+            return $http.get(url, config).then(success, error);
+        }
+    };
+});
+
+places_app.controller('PlacesController', function ($scope, $location, $anchorScroll, listsService, venuesService) {
     listsService.getLists().then(
         function (data) {
             $scope.lists = data;
@@ -63,34 +104,13 @@ places_app.controller('PlacesController', function ($scope, $http, $location, $a
         for (var offset = 0;
              offset < $scope.active_list.listItems.count;
              offset += places.LIST_PAGINATION_LIMIT) {
-            $http({
-                method: 'GET',
-                url: 'https://api.foursquare.com/v2/lists/' + $scope.active_list.id,
-                params: {
-                    oauth_token: places.OAUTH_TOKEN,
-                    v: places.FOURSQUARE_API_VERSION,
-                    limit: places.LIST_PAGINATION_LIMIT,
-                    offset: offset,
-                }
-            }).
-            success(function (data) {
-                angular.forEach(data.response.list.listItems.items, function (v, k) {
-                    var venue = v.venue;
-                    angular.forEach(venue.categories, function (c, _) {
-                        if (c.primary === true) {
-                            venue._places_primary_category = c;
-                        }
-                    });
-                    venue.location.latitude = venue.location.lat;
-                    venue.location.longitude = venue.location.lng;
-                    venue.location.latlng = new google.maps.LatLng(
-                        venue.location.lat, venue.location.lng);
-                    $scope.venues.push(venue);
+            venuesService.getVenues($scope.active_list.id, offset).then(
+                function (venues) {
+                    Array.prototype.push.apply($scope.venues, venues);
+                },
+                function (data) {
+                    console.log('error', data);
                 });
-            }).
-            error(function (data) {
-                console.log('error', data);
-            });
         }
     };
 
